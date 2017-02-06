78ab2b71743e21668901669e941f72739def2efc
Brian Leathem <bleathem@gmail.com>
2017-02-05 22:46:15 -0800

    Fixed the check for the TRAVIS_PULL_REQUEST env var

--- test/travis-environment.spec.js
+++ test/travis-environment.spec.js
@@ -54,5 +54,31 @@ describe('TravisEnvironment', function() {
       return travisEnvironment.checkTriggerRepo().should.be.rejected;
     });
   });
+
+  describe('#getDeployKey()', function() {
+    let travisEnvironment, _env;
+
+    beforeEach(function() {
+      travisEnvironment = new TravisEnvironment();
+      _env = _.cloneDeep(process.env);
+    });
+
+    afterEach(function() {
+      process.env = _.cloneDeep(_env);
+    });
+    it('should be rejected with no env vars set ', function() {
+      return travisEnvironment.getDeployKey().should.be.rejected;
+    });
+
+    it('should be rejected when the TRAVIS_PULL_REQUEST is true ', function() {
+      process.env.TRAVIS_PULL_REQUEST='false';
+      return travisEnvironment.getDeployKey().should.be.rejectedWith('Unable to retrieve the encryption key');
+    });
+
+    it('should be rejected when the TRAVIS_PULL_REQUEST is false ', function() {
+      process.env.TRAVIS_PULL_REQUEST='true';
+      return travisEnvironment.getDeployKey().should.be.rejectedWith('The travis ecrypted key var is not available to builds triggered by pull requests.  Aborting.');
+    });
+  });
 });
 
