6e50823729539a45808fb848f01f4948cdfd43d0
Brian Leathem <bleathem@gmail.com>
2017-02-05 22:31:16 -0800

    Fixed the borkin travis env init chaining

--- test/travis-environment.spec.js
+++ test/travis-environment.spec.js
@@ -23,14 +23,14 @@ describe('TravisEnvironment', function() {
       return travisEnvironment.checkTriggerRepo().should.be.rejected;
     });
     it('should be fulfilled with matching repos and slugs', function() {
-      process.env.TRAVIS_TAG='test';
+      process.env.TRAVIS='true';
       process.env.TRAVIS_REPO_SLUG='slug';
       process.env.TRAVIS_BRANCH='master';
       process.env.TRIGGER_REPO_SLUG='slug';
       process.env.TRIGGER_REPO_BRANCH='master';
       return travisEnvironment.checkTriggerRepo().should.be.fulfilled;
     });
-    it('should be rejected when TRAVIS_TAG is not set', function() {
+    it('should be rejected when TRAVIS is not set', function() {
       process.env.TRAVIS_REPO_SLUG='slug';
       process.env.TRAVIS_BRANCH='master';
       process.env.TRIGGER_REPO_SLUG='slug ';
@@ -38,7 +38,7 @@ describe('TravisEnvironment', function() {
       return travisEnvironment.checkTriggerRepo().should.be.rejected;
     });
     it('should be rejected with mismatching slugs', function() {
-      process.env.TRAVIS_TAG='test';
+      process.env.TRAVIS='true';
       process.env.TRAVIS_REPO_SLUG='slug';
       process.env.TRAVIS_BRANCH='master';
       process.env.TRIGGER_REPO_SLUG='slug2';
@@ -46,7 +46,7 @@ describe('TravisEnvironment', function() {
       return travisEnvironment.checkTriggerRepo().should.be.rejected;
     });
     it('should be rejected with mismatching repos', function() {
-      process.env.TRAVIS_TAG='test';
+      process.env.TRAVIS='true';
       process.env.TRAVIS_REPO_SLUG='slug';
       process.env.TRAVIS_BRANCH='master';
       process.env.TRIGGER_REPO_SLUG='slug';
